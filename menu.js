7f62d3a24e4cd5afd9d0fa82873c310e3bd0a40b
RLCHuncp <rlch06.uncp@gmail.com>
2020-08-30 19:48:40 -0500

    Adding typewriter effect

--- menu.js
+++ menu.js
@@ -1,6 +1,42 @@
-let menu = document.getElementById('enlaces');
-let hamburguesa =  document.getElementsByClassName('menu-header')[0];
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="nombre">Ruth Campos</i>',
+    ],
+    stringsElement: '#cadenas-texto',
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html', 
+});
+const typeds = new Typed('.typed-2', {
+    strings: [
+        '<i class="nombre">Front-end Developer</i>',
+        '<i class="nombre">Bach. Ingeniería de Sistemas</i>',
+    ],
+    stringsElement: '#cadenas-texto',
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html', 
+});
 
-hamburguesa.addEventListener('click', () =>{
-    menu.classList.toggle('extra-clase')
+// Redes Sociales
+const link = 'https://www.linkedin.com/in/ruthcampos'
+const facebook = document.getElementById('fb');
+facebook.addEventListener('click', () => {
+	link();
 })
